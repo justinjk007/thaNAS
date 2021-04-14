6b225693bf955a62eafe0cc971aa758c96dc221f
Justin-WorkMac <justinjoseph0007@gmail.com>
2021-04-14 08:53:55 -0400

    Setup dropbox uploading system

--- backup_config.sh
+++ backup_config.sh
@@ -1,16 +1,10 @@
 #!/bin/bash -x
 
-# Get access token, generate one if need too
-# https://www.dropbox.com/developers/apps/info/u908atkj2n0n8g7#settings
-
-ACCESS_TOKEN=""
+# Configure
+# https://github.com/andreafabrizi/Dropbox-Uploader
 
 cd ~/plex
 rm -rf thaNAS_config.7z
 7z a thaNAS_config ~/plex/config
 
-curl -X POST https://content.dropboxapi.com/2/files/upload \
-    --header "Authorization: Bearer $ACCESS_TOKEN" \
-    --header "Dropbox-API-Arg: {\"path\": \"/Backup/thaNAS_config.7z\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
-    --header "Content-Type: application/octet-stream" \
-    --data-binary @"thaNAS_config.7z"
+~/plex/dropbox_uploader.sh upload thaNAS_config.7z Backup/thaNAS_config.7z
