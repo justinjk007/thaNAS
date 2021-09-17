ce3a9ac0a0490cfc54afde43c296cd7072a7432a
Justin-WorkMac <justinjoseph0007@gmail.com>
2021-09-16 22:05:37 -0400

    Seed some items forever

--- remove_completed.sh
+++ remove_completed.sh
@@ -2,6 +2,8 @@
 
 HOST=192.168.0.10:9091
 
+KEEP_FOREVER_IDS="2" # seperate ID list by space
+
 # use transmission-remote to get torrent list from transmission-remote list
 # use sed to delete first / last line of output, and remove leading spaces
 # use cut to get first field from each line
@@ -18,12 +20,22 @@ do
   STATE_STOPPED=`transmission-remote $HOST -t $TORRENTID -i | grep "State: Stopped\|Finished\|Idle"`
   # if the torrent is "Stopped", "Finished", or "Idle" after downloading 100%…
   if [ "$DL_COMPLETED" != "" ] && [ "$STATE_STOPPED" != "" ]; then
-    # remove the torrent from Transmission and delete data.... data is linked to media dir anyways
+    # remove the torrent from Transmission
     echo "Torrent $TORRENTID is completed."
-    echo "Removing torrent from list."
-    transmission-remote $HOST -t $TORRENTID -rad
+    if [[ " $KEEP_FOREVER_IDS " =~ .*\ $TORRENTID\ .* ]]; then
+	echo "Torrent $TORRENTID is to be saved forever. Ignoring."
+    else
+	echo "Removing torrent from list."
+	transmission-remote $HOST -t $TORRENTID -rad
+    fi
   else
     echo "Torrent $TORRENTID is not completed. Ignoring."
   fi
   echo "* * * * * Operations on torrent ID $TORRENTID completed. * * * * *"
 done
+
+# get ids
+# transmission-remote 192.168.0.10:9091 --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited -d ' ' --fields=1
+
+# check id #2
+# transmission-remote 192.168.0.10:9091 -t 2 -i
